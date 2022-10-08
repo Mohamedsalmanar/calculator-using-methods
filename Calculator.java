ef0e6b6c69a8629fc4b3f8536a005b01c529c160
Salman <salmanwins@gmail.com>
2022-10-08 11:51:48 +0530

    Methods

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+class Calculator{
+
+public static void main(String[] args){
+Calculator calc = new Calculator();
+calc.add();
+calc.sub();
+calc.mul();
+calc.div();
+}
+void add(){
+System.out.println(10+20);
+}
+void sub(){
+System.out.println(20-10);
+}
+void mul(){
+System.out.println(10*20);
+}
+void div(){
+System.out.println(20/10);
+}
+}
